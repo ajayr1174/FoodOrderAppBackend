fdb7efbc2dac659f6e4339fad474643c891e6ede
Ajay Singh <ajayr1174@gmail.com>
2026-01-01 18:54:52 +0530

    initial checkin project setup and config

---
+++ src/main/java/com/foodorderappbackend/FoodOrderAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodorderappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodOrderAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foodorderappbackend/FoodOrderAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodorderappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
